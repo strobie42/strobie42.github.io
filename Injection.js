a50a6f2fb7b96c17c3f2a534f7dcecb95dfd33af
craig@strobie.com <strobie42>
2022-07-14 13:25:34 +0100

    Make height constraint check fire after web app screen rotation

--- Injection.js
+++ Injection.js
@@ -58,16 +58,22 @@ document.addEventListener(
   false
 );
 
-if (window.innerHeight < window.innerWidth * 0.55) {
-  // test if the CSS media rule didn't trigger
-  if (document.body.clientWidth == window.innerWidth) {
-    // KLUDGE force the height-constrained styles
-    document.body.style.maxWidth = String(1.9 * window.innerHeight) + "px";
-    let newFontSize = String(0.04 * window.innerHeight) + "px";
-    document.body.style.fontSize = newFontSize;
-    let button = document.getElementById("prevButton");
-    if (button != null) button.style.fontSize = newFontSize;
-    button = document.getElementById("nextButton");
-    if (button != null) button.style.fontSize = newFontSize;
+// This check for height constraints needs to happen *after* screen
+// rotation for the web app (height and width are initially zero and
+// then, after that, set for portrait, before finally being set for
+// landscape).
+onload = onresize = function () {
+  if (window.innerHeight < window.innerWidth * 0.55) {
+    // test if the CSS media rule didn't trigger
+    if (document.body.clientWidth == window.innerWidth) {
+      // KLUDGE force the height-constrained styles
+      document.body.style.maxWidth = String(1.9 * window.innerHeight) + "px";
+      let newFontSize = String(0.04 * window.innerHeight) + "px";
+      document.body.style.fontSize = newFontSize;
+      let button = document.getElementById("prevButton");
+      if (button != null) button.style.fontSize = newFontSize;
+      button = document.getElementById("nextButton");
+      if (button != null) button.style.fontSize = newFontSize;
+    }
   }
 }
